b99b1fb00d15c35b3aa6c5c190fef0f8365c9bfe
Laurent WITT <laurent.witt@gmail.com>
2018-01-02 11:34:38 +0100

    switched to node-red

--- j5net-mqtt2db.js
+++ j5net-mqtt2db.js
@@ -18,74 +18,74 @@ module.exports = (app) => {
 
       var broker = app.get("mqtt_broker");
 
-      broker.subscribe(app.get("config").mqtt_node_base+"#");
-
-      broker.on("message", function(topic,data) {
-
-
-            if (topic.startsWith(app.get("config").mqtt_node_base)) {
-
-                  var id = topic.split("/")[3];
-
-                  if (id && parseInt(id) && (parseInt(id)<100 || parseInt(id)>999)) {
-
-                        console.log((new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + " - [database] node=" + id + " " + data).yellow);
-
-                        models.node.findOne({id : id}, function (err, res){
-                              if (!err) {
-                                    // existing node
-                                    if (res) {
-                                          res.lastUpdate = new Date();
-                                          res.lastData = JSON.parse(data.toString());
-                                          res.save(function(err) {
-                                                if (err)
-                                                console.log(err);
-                                          });
-
-                                          // ugly object clone :(
-                                          var obj = JSON.parse(JSON.stringify(res));
-                                          delete obj._id;
-                                          delete obj.__v;
-                                          if (nodeinfos[id]) {
-                                                obj.name = nodeinfos[id].name;
-                                          }
-                                          else {
-                                                obj.name = "unknown";
-                                          }
-                                          nodes[id] = obj;
-                                    }
-                                    else {
-                                          // node to be created
-                                          var n1 = new models.node({ id: id, lastData : JSON.parse(data)});
-                                          n1.save(function(err) {
-                                                if (err)
-                                                console.log(err);
-                                          });
-
-                                          // TODO ugly object clone :(
-                                          var obj = JSON.parse(JSON.stringify(n1));
-                                          delete obj.id;
-                                          delete obj._id;
-                                          if (nodeinfos[id]) {
-                                                obj.name = nodeinfos[id].name;
-                                          }
-                                          else {
-                                                obj.name = "unknown";
-                                          }
-                                          nodes[id] = obj;
-                                    }
-                              }
-                              else {
-                                    console.log("error : " + err);
-                              }
-                        });
-
-                        var nodedata = new models.nodedata({ id: id, data: JSON.parse(data)});
-
-                        nodedata.save(function (err, savednode) {
-                              if (err) console.error(err);
-                        });
-                  }
-            }
-      });
+      // broker.subscribe(app.get("config").mqtt_node_base+"#");
+      //
+      // broker.on("message", function(topic,data) {
+      //
+      //
+      //       if (topic.startsWith(app.get("config").mqtt_node_base)) {
+      //
+      //             var id = topic.split("/")[3];
+      //
+      //             if (id && parseInt(id) && (parseInt(id)<100 || parseInt(id)>999)) {
+      //
+      //                   console.log((new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString() + " - [database] node=" + id + " " + data).yellow);
+      //
+      //                   models.node.findOne({id : id}, function (err, res){
+      //                         if (!err) {
+      //                               // existing node
+      //                               if (res) {
+      //                                     res.lastUpdate = new Date();
+      //                                     res.lastData = JSON.parse(data.toString());
+      //                                     res.save(function(err) {
+      //                                           if (err)
+      //                                           console.log(err);
+      //                                     });
+      //
+      //                                     // ugly object clone :(
+      //                                     var obj = JSON.parse(JSON.stringify(res));
+      //                                     delete obj._id;
+      //                                     delete obj.__v;
+      //                                     if (nodeinfos[id]) {
+      //                                           obj.name = nodeinfos[id].name;
+      //                                     }
+      //                                     else {
+      //                                           obj.name = "unknown";
+      //                                     }
+      //                                     nodes[id] = obj;
+      //                               }
+      //                               else {
+      //                                     // node to be created
+      //                                     var n1 = new models.node({ id: id, lastData : JSON.parse(data)});
+      //                                     n1.save(function(err) {
+      //                                           if (err)
+      //                                           console.log(err);
+      //                                     });
+      //
+      //                                     // TODO ugly object clone :(
+      //                                     var obj = JSON.parse(JSON.stringify(n1));
+      //                                     delete obj.id;
+      //                                     delete obj._id;
+      //                                     if (nodeinfos[id]) {
+      //                                           obj.name = nodeinfos[id].name;
+      //                                     }
+      //                                     else {
+      //                                           obj.name = "unknown";
+      //                                     }
+      //                                     nodes[id] = obj;
+      //                               }
+      //                         }
+      //                         else {
+      //                               console.log("error : " + err);
+      //                         }
+      //                   });
+      //
+      //                   var nodedata = new models.nodedata({ id: id, data: JSON.parse(data)});
+      //
+      //                   nodedata.save(function (err, savednode) {
+      //                         if (err) console.error(err);
+      //                   });
+      //             }
+      //       }
+      // });
 };
